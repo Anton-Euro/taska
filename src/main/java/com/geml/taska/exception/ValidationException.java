0547928915e7cac1b37a62bd14add83691947b1f
Geml <pirigovanton906@gmail.com>
2025-03-25 13:31:09 +0300

    fix

--- src/main/java/com/geml/taska/exception/ValidationException.java
+++ src/main/java/com/geml/taska/exception/ValidationException.java
@@ -2,13 +2,16 @@ package com.geml.taska.exception;
 
 import java.util.List;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
+@Slf4j
 public class ValidationException extends RuntimeException {
     private final List<String> errors;
 
     public ValidationException(List<String> errors) {
         super("Validation failed");
         this.errors = errors;
+        log.error("Validation failed with errors: {}", errors);
     }
 }
